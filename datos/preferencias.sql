8990848c2a892f64711a7ebd7f3cbb929d60d5b7
mishellcastillo-wq <mishell.castillo@unl.edu.ec>
2026-07-23 17:34:50 -0500

    feat: agregar carpeta datos

---
+++ datos/preferencias.sql
@@ -0,0 +1,11 @@
+INSERT INTO Preferencia (nombre) VALUES
+('libros'), ('snoopy'), ('humor'), ('formula1'), ('programacion'),
+('BTS'), ('mundial2026'), ('maquillaje'), ('kpop'),
+('WoW'), ('IA'), ('existencialismo'), ('manhwa'), ('musica'),
+('dance'), ('deporte'), ('contenido'), ('fitness');
+
+INSERT INTO UsuarioPreferencia (id_usuario, id_preferencia) VALUES
+(1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
+(3, 6), (3, 7), (3, 8), (3, 9),
+(2, 10), (2, 11), (2, 12), (2, 13), (2, 3), (2, 6), (2, 14),
+(4, 15), (4, 16), (4, 17), (4, 18);
---
+++ datos/usuarios.sql
@@ -0,0 +1,5 @@
+INSERT INTO Usuario (nombre, email, contrasena) VALUES
+('Mishell Castillo', 'mishell@gmail.com', 'mc123'),
+('Maria Rodriguez', 'maria@gmail.com', 'mr123'),
+('Teresa Rivas', 'teresa@gmail.com', 'tr123'),
+('Alisson Gaona', 'alisson@gmail.com', 'ag123');
