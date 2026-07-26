3002bfcd575dc95393f5132dc3a9987083030b28
Alisson Gaona <alisson.gaona@unl.ec>
2026-07-26 12:11:10 -0500

    feat: Insertar datos de los nuevos videos en videos.sql

--- datos/videos.sql
+++ datos/videos.sql
@@ -1,2 +1,14 @@
 INSERT INTO video (titulo, autor, url_video, descripcion, likes)
 VALUES ('Minahonda', 'bluebul', 'videos/video1.mp4', '#wow', 0); 
+
+INSERT INTO video (titulo, autor, url_video, descripcion, likes)
+VALUES ('Olympics', 'Hoku.Blend', 'videos/video2.mp4', '#deporte', 50);  
+
+INSERT INTO video (titulo, autor, url_video, descripcion, likes)
+VALUES ('Salsa', 'ColombiaSalsa', 'videos/video3.mp4', '#dance', 10);   
+
+INSERT INTO video (titulo, autor, url_video, descripcion, likes)
+VALUES ('GymVlog', 'FitnessCinemastic', 'videos/video4.mp4', '#fitness', 20);   
+
+INSERT INTO video (titulo, autor, url_video, descripcion, likes)
+VALUES ('ContenidoEnTendencia', 'GlowAgencia', 'videos/video5.mp4', '#contenido', 35);   
